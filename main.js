473bd593302742c81882329df68adb88957f547d
Kuba Potera <Kubaz2k@gmail.com>
2021-05-07 20:54:03 -0400

    added functionality to create new blackjack game model

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 var deck = new Array();
 
+//will push objects containing players and hands, 
+//with player 0 always being Luigi, with a max of 5 players(again including Luigi)
+var activeGames = [];
+
 for (var i = 0; i < suits.length; i++) {
 	for (var x = 0; x < values.length; x++) {
 		var card = { Value: values[x], Suit: suits[i] };
@@ -28,15 +32,23 @@ client.once('ready', () => {
 });
 
 client.on("message", message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) { return;}
 	if (message.channel.name == "bot-testing") {
 		console.log(`${message.author}: ${message.content}`);
-		if (message.content.toLowerCase() === `${prefix}hello luigi`) {
+		if (message.content.toLowerCase().slice(prefix.length) === `hello luigi`) {
 			message.channel.send(`Hello ${message.author}`)
 		}
-}
+		if (message.content.toLowerCase().slice(prefix.length) === `blackjack`) {
+			activeGames.push({ 'Players': [message.author], 'deckPos': 0 });
+			console.log(activeGames);
+		}
+		if (message.content.toLowerCase().slice(prefix.length).startsWith(`blackjack join `)) {
+
+		}
+	}
 });
 
 
 
-
-client.login('ODM5OTMxNjk1MzU2NzA2ODI3.YJQ1WQ.Fsu8RDg8BWfwUn_ls_tSzTLO - L4');
+//Replace with new bot token
+client.login('Replace with new bot token');
