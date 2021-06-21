3d289ef1b2b787b1e79662ff601180461b9f6435
Kuba Potera <Kubaz2k@gmail.com>
2021-06-21 08:09:22 -0400

    made further changes to bot, implements start command to make a deep copy of deck, shuffle, and deal starting hands to players via direct messages

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var deck = new Array();
 
 //will push objects containing players and hands, 
 //with player 0 always being Luigi, with a max of 5 players(again including Luigi)
-var activeGames = [];
+var activeGames = {};
 
 for (var i = 0; i < suits.length; i++) {
 	for (var x = 0; x < values.length; x++) {
@@ -23,8 +23,24 @@ function handCheck() {
 
 }
 
+function shuffle(array) {
+	for (let i = 0; i < 10; i++) {
+		array.sort(() => Math.random() - 0.5);
+	}
+}
 
 
+function play(game) {//deals cards and checks player hands, hopefully doesn't just clog up instantly and can handle multiple games...
+	for (player in game.Players) {
+		game.Players[player].hand = [];
+		game.Players[player].hand.push(game.deck[game.deckPos]);
+		game.deckPos++;
+		game.Players[player].hand.push(game.deck[game.deckPos]);
+		game.deckPos++;
+		console.log(game.Players[player].hand);
+		game.Players[player].send(JSON.stringify(game.Players[player].hand));
+	}
+}
 
 
 client.once('ready', () => {
@@ -36,14 +52,37 @@ client.on("message", message => {
 	if (message.channel.name == "bot-testing") {
 		console.log(`${message.author}: ${message.content}`);
 		if (message.content.toLowerCase().slice(prefix.length) === `hello luigi`) {
-			message.channel.send(`Hello ${message.author}`)
+			message.channel.send(`Hello ${message.author}`);
 		}
 		if (message.content.toLowerCase().slice(prefix.length) === `blackjack`) {
-			activeGames.push({ 'Players': [message.author], 'deckPos': 0 });
+			activeGames[message.author.username] = {'Players': [message.author], 'deckPos': 0, 'active': false };
+			message.channel.send(`Welcome to the table ${message.author.username}, invite your friends or start playing by yourself with $blackjack start`);
 			console.log(activeGames);
 		}
 		if (message.content.toLowerCase().slice(prefix.length).startsWith(`blackjack join `)) {
-
+			let name = message.content.slice(16);
+			if (activeGames.hasOwnProperty(name)) {
+				if (activeGames[name].Players.length >= 4) {
+					message.channel.send(`Sorry, that table's already full ${message.author.username}`)
+				} else if (activeGames[name].active == true) {
+					message.channel.send(`You snooze you lose my friend, this game is already running!`)
+				} else {
+					activeGames[name].Players.push(message.author);
+					message.channel.send(`Welcome to the table ${message.author.username}, I'll be starting the game soon`)
+				}
+			}
+			console.log(activeGames);
+		}
+		if (message.content.toLocaleLowerCase().slice(prefix.length) === 'blackjack start') {
+			if (activeGames.hasOwnProperty(message.author.username)) {
+				activeGames[message.author.username].active = true;
+				activeGames[message.author.username].deck = JSON.parse(JSON.stringify(deck));
+				shuffle(activeGames[message.author.username].deck);
+				console.log(activeGames[message.author.username]);
+				play(activeGames[message.author.username]);
+			} else {
+				message.channel.send(`Sorry ${message.author.username} but you have to pick a table with "$blackjack", or join someone elses table with "$blackjack join" first!`);
+			}
 		}
 	}
 });
@@ -51,4 +90,4 @@ client.on("message", message => {
 
 
 //Replace with new bot token
-client.login('Replace with new bot token');
+client.login('ODM5OTMxNjk1MzU2NzA2ODI3.YJQ1WQ.K6xTPMa8RaozIC9jIdGEhuvO3zQ');
